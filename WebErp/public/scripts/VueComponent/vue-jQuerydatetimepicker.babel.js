07cbd882e46ca87811c1f5899c5ada81672c0553
Andy Hsu <hamowe527@gmail.com>
2017-12-18 02:58:15 +0800

    -update

--- WebErp/public/scripts/VueComponent/vue-jQuerydatetimepicker.babel.js
+++ WebErp/public/scripts/VueComponent/vue-jQuerydatetimepicker.babel.js
@@ -13,18 +13,21 @@
  */
 Vue.component("vue-datetimepicker", {
 
-    template: "\n    <input type='text' v-bind:placeholder=\"placeholder\" />\n        ",
+    template: "\n    <input type='text' v-bind:placeholder=\"placeholderText\" />\n        ",
     props: {
         changecallback: null,
         timepicker: {
             type: Boolean
+        },
+        placeholder: {
+            type: String
         }
     },
     data: function data() {
         // use function to make reset function work
         return {
             value: '',
-            placeholder: "",
+            placeholderText: "",
             format: ""
         };
     },
@@ -40,69 +43,10 @@ Vue.component("vue-datetimepicker", {
     mounted: function mounted() {
         var vm = this;
         if (this.timepicker) {
-            this.placeholder = "YYYY/MM/DD HH:MM";
+            this.placeholderText = this.placeholder == null ? "YYYY/MM/DD HH:MM" : this.placeholder;
             this.format = "Y/m/d H:i";
         } else {
-            this.placeholder = "YYYY/MM/DD";
-            this.format = "Y/m/d";
-        }
-        var mycomp = $(this.$el).datetimepicker({
-            format: this.format,
-            onChangeDateTime: function onChangeDateTime(dp, $input) {
-                vm.value = $input.val();
-                vm.$emit('input', vm.value);
-                if (typeof vm.changecallback == 'function') {
-                    vm.changecallback();
-                }
-            },
-            timepicker: this.timepicker
-        });
-    }
-});
-/*!
- * 
- * 20170608 Andy Hsu
- * jQuery-datetimepicker
- * Global constant:
- * js file:
- *  jQuery
- *  ...
- * style:
- *  ...
- */
-Vue.component("vue-datetimepicker", {
-
-    template: "\n    <input type='text' v-bind:placeholder=\"placeholder\" />\n        ",
-    props: {
-        changecallback: null,
-        timepicker: {
-            type: Boolean
-        }
-    },
-    data: function data() {
-        // use function to make reset function work
-        return {
-            value: '',
-            placeholder: "",
-            format: ""
-        };
-    },
-    methods: {
-        onClose: function onClose(date) {
-            this.$emit('input', date);
-        },
-        setValue: function setValue(date) {
-            $(this.$el).val(date);
-        }
-    },
-    created: function created() {},
-    mounted: function mounted() {
-        var vm = this;
-        if (this.timepicker) {
-            this.placeholder = "YYYY/MM/DD HH:MM";
-            this.format = "Y/m/d H:i";
-        } else {
-            this.placeholder = "YYYY/MM/DD";
+            this.placeholderText = this.placeholder == null ? "YYYY/MM/DD HH:MM" : this.placeholder;
             this.format = "Y/m/d";
         }
         var mycomp = $(this.$el).datetimepicker({
