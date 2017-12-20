abbef17c4a181a9116525a820d331984dd88a223
Andy Hsu <hamowe527@gmail.com>
2017-12-20 14:07:50 +0800

    Framework update & path update

--- ErpBaseLibrary/DB/ConnObj.cs
+++ ErpBaseLibrary/DB/ConnObj.cs
@@ -9,6 +9,6 @@ namespace ErpBaseLibrary.DB
     }
     public class MyConnStringList
     {
-        public static string AzureGoodeasy = ConfigurationManager.ConnectionStrings["ConnAzureGoodeasy"].ConnectionString;
+        public static string AzureGoodeasy = ConfigurationManager.ConnectionStrings["goodeasyConnectionString"].ConnectionString;
     }
 }
