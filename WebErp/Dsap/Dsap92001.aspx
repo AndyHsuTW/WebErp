d00384c1b4c3bc2e7437ec400c17160327f5e9aa
zx2538265 <zx2538265@gmail.com>
2017-12-21 21:58:08 +0800

    Add Dsap92001

---
+++ WebErp/Dsap/Dsap92001.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/master/BaseMasterPage.master" AutoEventWireup="true" CodeFile="Dsap92001.aspx.cs" Inherits="Dsap_Dsap92001" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+     <script>
+         (function () {
+             requirejs.config({
+                 paths: {
+                     "Dsap92001": "Dsap/Dsap92001",
+                 },
+                 shim: {}
+             });
+
+             var requiredFiles = ["Dsap92001"];
+
+             function onLoaded(dcnp00501) {
+             }
+
+             function onError(error) {
+                 console.error(error);
+             }
+
+             require(requiredFiles, onLoaded, onError);
+         })();
+    </script>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+
+</asp:Content>
+
---
+++ WebErp/Dsap/Dsap92001.js
@@ -0,0 +1,33 @@
+﻿'use strict';
+(function () {
+    requirejs.config({
+        paths: {
+            "functionButton": isIE ? "VueComponent/FunctionButton.babel" : "VueComponent/FunctionButton",
+            "uibPagination": "public/scripts/VueComponent/vuejs-uib-pagination",
+            "vueDatetimepicker": isIE ? "public/scripts/VueComponent/vue-jQuerydatetimepicker.babel" : "public/scripts/VueComponent/vue-jQuerydatetimepicker",
+            "jqueryDatetimepicker": "public/scripts/jquery.datetimepicker/jquery.datetimepicker.full",
+            "jqueryDatetimepicker-css": "public/scripts/jquery.datetimepicker/jquery.datetimepicker",
+            "jquery-mousewheel": "public/scripts/jquery.mousewheel.min",
+        },
+        shim: {
+            "jqueryDatetimepicker": {
+                "deps": ["css!jqueryDatetimepicker-css"]
+            },
+            "vueDatetimepicker": {
+                "deps": ['jqueryDatetimepicker']
+            },
+        }
+    });
+
+    var requiredFiles = ["bootstrap", "functionButton", "uibPagination", "vueDatetimepicker", "LoadingHelper"];
+
+    function onLoaded(bootstrap, functionButton, uibPagination, vueDatetimepicker, loadingHelper) {
+       
+    }
+
+    function onError(error) {
+        console.error(error);
+    }
+
+    define(requiredFiles, onLoaded, onError);
+})();
