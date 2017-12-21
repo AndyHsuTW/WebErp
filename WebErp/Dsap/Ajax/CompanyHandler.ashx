d5e84fda7fdca7898bc01a0abd61f5fe55a25741
zx2538265 <zx2538265@gmail.com>
2017-12-22 00:24:03 +0800

    Dsap92001

---
+++ WebErp/Dsap/Ajax/CompanyHandler.ashx
@@ -0,0 +1,75 @@
+﻿<%@ WebHandler Language="C#" Class="CompanyHandler" %>
+using System;
+using System.Web;
+using Newtonsoft.Json;
+using ErpBaseLibrary.DB;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+public class CompanyHandler : IHttpHandler
+{
+
+    public void ProcessRequest(HttpContext context)
+    {
+        context.Response.ContentType = "text/plain";
+
+
+        var List = new List<Company>();
+
+        using (var conn = new SqlConnection(MyConnStringList.AzureGoodeasy))
+        {
+            conn.Open();
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = @"
+            SELECT 
+                cnf1002_fileorder AS code, 
+                cnf1003_char01 AS name
+            FROM [dbo].[cnf10]
+            WHERE cnf1001_filetype ='D002' and  cnf1005_char03 ='1'
+            ";
+            using (var rd = cmd.ExecuteReader())
+            {
+                if (rd.HasRows)
+                {
+                    while (rd.Read())
+                    {
+                        List.Add(new Company()
+                        {
+                            Name = rd["name"].ToString(),
+                            Code = rd["code"].ToString()
+                            
+                        });
+                        
+                    }
+                }
+
+            }
+
+        }
+
+        context.Response.Write(JsonConvert.SerializeObject(List));
+        
+
+    }
+
+
+
+
+    public class FilterOption
+    {
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+    public class Company
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+    }
+    public bool IsReusable
+    {
+        get
+        {
+            return false;
+        }
+    }
+
+}
--- WebErp/Dsap/Dsap92001.js
+++ WebErp/Dsap/Dsap92001.js
@@ -22,7 +22,66 @@
     var requiredFiles = ["bootstrap", "functionButton", "uibPagination", "vueDatetimepicker", "LoadingHelper"];
 
     function onLoaded(bootstrap, functionButton, uibPagination, vueDatetimepicker, loadingHelper) {
-       
+        window.dsap92001 = new Vue({
+            el: "#Dsap92001",
+            data: {
+                sendAjax:null,
+                Filter: {
+                   
+                    StartDate: null,
+                    EndDate: null,
+                   
+                },
+                CompanyList: [],
+                IsCheckAll: false
+            },
+            methods: {
+                OnSearch: function () {
+                    console.log("OnSearch");
+                    var filterOption = {
+                        StartDate: this.Filter.StartDate,
+                        EndDate: this.Filter.EndDate
+                    }
+
+                    if (this.sendAjax != null) {
+                        this.sendAjax.abort();
+                    }
+                    LoadingHelper.showLoading();
+                    this.sendAjax = $.ajax({
+                        url: rootUrl + "Dsap/Ajax/CompanyHandler.ashx",
+                        cache: false,
+                        async: true,
+                        customData: {
+                            vueObj: this
+                        },
+                        success: function (result) {
+                            LoadingHelper.hideLoading();
+                            this.customData.vueObj.CompanyList = JSON.parse(result);
+                            if (this.customData.vueObj.CompanyList.length == 0) {
+                                alert("查無資料");
+                            }
+                        }, error: function (jqXhr, textStatus, errorThrown) {
+                            if (jqXhr.status == 0) {
+                                return;
+                            }
+                            LoadingHelper.hideLoading();
+                            console.error(textStatus);
+                            alert("查詢失敗");
+                        }
+                    })
+
+                },OnExport: function () {
+                    alert("B")
+                }, OnCheckAll: function () {
+                    Vue.nextTick(function () {
+                        console.log(dsap92001.IsCheckAll ? "true" : "false");
+                        for (var i in dsap92001.CompanyList) {
+                            dsap92001.CompanyList[i].checked = dsap92001.IsCheckAll;
+                        }
+                    });
+                },
+            }
+        })
     }
 
     function onError(error) {
