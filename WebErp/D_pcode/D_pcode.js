813f70f080f3b9b1b06491d1cb2b557b178de023
zx2538265 <zx2538265@gmail.com>
2017-12-28 22:18:36 +0800

    Add D_pcode

---
+++ WebErp/D_pcode/D_pcode.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+(function (window) {
+
+    window.D_pcode = window.D_pcode || {};
+
+    var D_pcode = window.D_pcode;
+
+    D_pcode.Init = function (button) {
+
+
+
+
+    };
+
+
+
+})(window);
